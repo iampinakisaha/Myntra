bb8cf16d8d15f953426474113f39c8ee17c028e3
iampinakisaha <pinaki.saha93@gmail.com>
2024-06-26 20:58:20 +0530

    project complete as of now

--- myntra-react-clone-ver-1/src/components/Header.jsx
+++ myntra-react-clone-ver-1/src/components/Header.jsx
@@ -1,59 +1,60 @@
-import React from 'react';
+import React from "react";
 import { IoPerson } from "react-icons/io5";
 import { GiHearts } from "react-icons/gi";
 import { BsBagFill } from "react-icons/bs";
-
-import { Link } from 'react-router-dom';
-
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const bag = useSelector((store) => store.bag);
+  console.log("Bag containes", bag);
   return (
     <header>
-        <div className="logo_container">
-          <Link to="/">
-            <img
-              className="myntra_home"
-              src="images/myntra_logo.webp"
-              alt="Myntra Home"
-            />
-          </Link>
-        </div>
-        <nav className="nav_bar">
-          <a href="#">Men</a>
-          <a href="#">Women</a>
-          <a href="#">Kids</a>
-          <a href="#">Home & Living</a>
-          <a href="#">Beauty</a>
-          <a href="#">
-            Studio <sup>New</sup>
-          </a>
-        </nav>
-        <div className="search_bar">
-          <span className="material-symbols-outlined search_icon">search</span>
-          <input
-            className="search_input"
-            placeholder="Search for products, brands and more"
+      <div className="logo_container">
+        <Link to="/">
+          <img
+            className="myntra_home"
+            src="images/myntra_logo.webp"
+            alt="Myntra Home"
           />
-        </div>
-        <div className="action_bar">
-          <div className="action_container">
+        </Link>
+      </div>
+      <nav className="nav_bar">
+        <a href="#">Men</a>
+        <a href="#">Women</a>
+        <a href="#">Kids</a>
+        <a href="#">Home & Living</a>
+        <a href="#">Beauty</a>
+        <a href="#">
+          Studio <sup>New</sup>
+        </a>
+      </nav>
+      <div className="search_bar">
+        <span className="material-symbols-outlined search_icon">search</span>
+        <input
+          className="search_input"
+          placeholder="Search for products, brands and more"
+        />
+      </div>
+      <div className="action_bar">
+        <div className="action_container">
           <IoPerson />
-            <span className="action_name" >Profile</span>
-          </div>
-
-          <div className="action_container">
-          <GiHearts />
-            <span className="action_name">Wishlist</span>
-          </div>
-
-          <Link className="action_container" to="/bag">
-          <BsBagFill />
-            <span className="action_name">Bag</span>
-            <span className="bag-item-count">0</span>
-          </Link>
+          <span className="action_name">Profile</span>
         </div>
-      </header>
-  )
-}
 
-export default Header
+        <div className="action_container">
+          <GiHearts />
+          <span className="action_name">Wishlist</span>
+        </div>
+
+        <Link className="action_container" to="/bag">
+          <BsBagFill />
+          <span className="action_name">Bag</span>
+          <span className="bag-item-count">{bag.length}</span>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
